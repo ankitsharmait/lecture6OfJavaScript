f81b4774e7525c2e6a413e87a0482bdf300808e9
Ankit <ankitsharmabela@gmail.com>
2024-11-16 10:08:13 +0530

    crud in javascript lecture 6.

--- lecture6.js
+++ lecture6.js
@@ -1,6 +1,20 @@
-console.log("hello javascript");
+/*console.log("hello javascript");
 const html=document.children[0];
 const body=html.children[1];
 const h =body.children[0];
 //console.log(body.innerHTML);
-console.log(h.innerHTML);
+//console.log(h.innerHTML);
+console.log(html);*/
+/*const body = document.querySelector("body");
+console.log(body.innerHTML);
+const ptag = document.querySelector("p");
+console.log(ptag.textContent);*/
+
+/*const ptag = document.querySelector("p");
+console.log(ptag.textContent);*/
+
+//how to add <li> inside <ul>
+  const liel=document.createElement("li");
+  liel.innerText=" i am first list item";
+  const ul = document.querySelector("ul");
+  ul.appendChild(liel);
